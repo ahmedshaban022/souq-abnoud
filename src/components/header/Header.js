109ca34625e33ac158b93d86d72fd22098b23909
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2022-07-08 03:21:59 +0200

    Beta 1.0.0

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
   const handleLogout=()=>{
     localStorage.clear();
     setIsAdmin(false);
-    window.location.replace("/")
+    window.location.replace("souq-abnoud/")
 
   }
   return (
