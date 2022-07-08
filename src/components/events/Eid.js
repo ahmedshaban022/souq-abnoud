118b6e618098669869c726e874a98b9aa994c4ac
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2022-07-08 13:17:50 +0200

    Eid Adha

---
+++ src/components/events/Eid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Eid.css"
+const Eid = () => {
+  return (
+    <div className='eid'>
+
+  <img className="zagrafa" src="https://i.suar.me/qWZlJ/l" alt="zagrafa" />
+  <img className="zagrafa2" src="https://i.suar.me/qWZlJ/l" alt="zagrafa" />
+  <img className="zagrafa3" src="https://i.suar.me/qWZlJ/l" alt="zagrafa" />
+  <span className="one">عيد</span>
+  <span className="two">سعيد</span>
+
+
+
+    </div>
+  )
+}
+
+export default Eid
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Carousal from '../Carousal'
 import Gallerys from '../Categories'
+import Eid from '../events/Eid'
 import Products from '../products/Products'
 
 const Home = () => {
   return (
     <div className='homePage'>
+      <div>
+        <Eid/>
+      </div>
       <div  className=''>
 
       <Carousal/>
       </div>
+
       <div className='mt-5'>
 
       <Gallerys/>
