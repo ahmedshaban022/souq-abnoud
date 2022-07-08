9e96c14c32a8878e393a5728046eec68f533a4ca
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2022-07-08 02:06:48 +0200

    change the carousal images

--- src/components/Carousal.js
+++ src/components/Carousal.js
@@ -1,32 +1,41 @@
 import React, { useState } from 'react'
 import Loader from './loader/Loader';
 
+
 const Carousal = () => {
 
-    const [carousalImgs,setCarousalImgs]=useState([
-        "https://www.colgate.com/content/dam/cp-sites/oral-care/oral-care-center-relaunch/en-us/general/brands/colgate-kids-regimen.png",
-        "https://www.suave.com/sk-eu/content/dam/brands/suave/united_states_ofamerica/1232102-kids-sclp-originals-producthighlight-1440x1000-july2018.png.rendition.991.705.png",
-        "https://img3.exportersindia.com/product_images/bc-full/dir_91/2706008/kids-products-1744146.jpg",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWuGsfT8GXRdsarX5vAMIclhfqVqfmq-CNXCa360CV5rRI7pieepyiBkVJCfvW_QOICFw&usqp=CAU",
-    ]);
+    // const [carousalImgs,setCarousalImgs]=useState([img1,img2,img3,img4,img5]);
 
 
   return (
     <div
   id="carouselExampleControls"
-  className="carousel slide container "
+  className="carousel slide container mt-3"
   data-bs-ride="carousel"
 >
   <div className="carousel-inner ">
-    {
-        carousalImgs ? carousalImgs.map((img,index)=>(
-            <div className={`carousel-item ${index===0?"active":""}`} key={index}>
-      <img src={img} className="d-block w-100" alt="..." />
+    
+       
+      <div className={`carousel-item active `} >
+      <img src={require("../images/a1.jpeg")} className="d-block w-100" alt="..." />
     </div>
-        )):<div className='mt-3 p-3 d-flex justify-content-center'> 
-        <Loader/>
-      </div>
-    }
+
+     <div className={`carousel-item `} >
+     <img src={require('../images/a2.jpeg')} className="d-block w-100" alt="..." />
+   </div>
+
+   <div className={`carousel-item `} >
+     <img src={require('../images/a3.jpeg')} className="d-block w-100" alt="..." />
+   </div>
+
+   <div className={`carousel-item `} >
+     <img src={require('../images/a4.jpeg')} className="d-block w-100" alt="..." />
+   </div>
+   <div className={`carousel-item `} >
+     <img src={require('../images/a5.jpeg')} className="d-block w-100" alt="..." />
+   </div>
+        
+    
   </div>
   <button
     className="carousel-control-prev"
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' p-3 text-center text-white' style={{backgroundColor:"teal"}}> Developed By 𝒜ℋℳℰ𝒟 𝒮ℋ𝒜ℬ𝒜𝒩</div>
+    <div className=' p-3 text-center text-white ' style={{backgroundColor:"teal"}}> Developed By <a className='text-decoration-none text-white' href='https://ahmedshaban022.github.io/my-portfolio/' target={"_blank"}> 𝒜ℋℳℰ𝒟 𝒮ℋ𝒜ℬ𝒜𝒩</a></div>
   )
 }
 
