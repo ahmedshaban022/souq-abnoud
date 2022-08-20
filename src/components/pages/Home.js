fde0419152df3b0e6c2be5e9a55164add7c102f1
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2022-08-20 14:30:44 +0200

    make scroll to top and theam btn position fixed

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,15 +3,37 @@ import { toast } from 'react-toastify'
 import { Globalstate } from '../../Globalstate'
 import Carousal from '../Carousal'
 import Gallerys from '../Categories'
-import Eid from '../events/Eid'
+
 import Products from '../products/Products'
 
 const Home = () => {
   const state=useContext(Globalstate);
-  const [theam]=state.theam
+  const [theam,setTheam]=state.theam;
+
+
+
+ const handleChangeTheam=()=>{
+    localStorage.setItem('theam',theam==="light"?"dark":"light");
+    setTheam(theam==="light"?"dark":"light");
+  }
   return (
     <div className='homePage' style={{backgroundColor:theam==="dark"?"black":"white"}}>
     
+    <div className='rounded-circle ' style={{position:"fixed",right:'5px',width:"40px",height:"40px",zIndex:5,backgroundColor:theam==="dark"?"black":"black"}}>
+
+<button className='btn text-white fw-bold' style={{border:"none"}} onClick={handleChangeTheam}>{theam ==="light"?<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+      <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+      <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
+    </svg>
+    :
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-sun" viewBox="0 0 16 16">
+      <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+    </svg>}
+    </button>
+      </div>
+    
+
+
       <div  className=''>
 
       <Carousal/>
@@ -22,7 +44,13 @@ const Home = () => {
       <Gallerys/>
       </div>
       <Products  />
-
+      <div className=' ' style={{position:"fixed",right:'3px',bottom:"20px",zIndex:3}}>
+          <button className='btn ' style={{border:"none"}} onClick={()=>window.scrollTo(0,0)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+          </svg>
+          </button>
+      </div>
     </div>
   )
 }
