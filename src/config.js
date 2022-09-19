c75132120d7ea320317ae6516a6ecf9fb46c40c3
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2022-09-19 12:09:25 +0200

    change server to render.com

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const config={
     // url:"https://souq-abnoud.vercel.app/",
-    url:"https://souq-abnod.herokuapp.com/"
+    // url:"https://souq-abnod.herokuapp.com/",
+    url:"https://souq-abnoud.onrender.com/",
 }
