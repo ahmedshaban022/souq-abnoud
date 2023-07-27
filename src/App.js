a2fc3ecc1fce50e706a877123ac7de40cb9d2b48
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2023-07-27 14:56:05 +0300

    fix the bootstrap bug

--- src/App.js
+++ src/App.js
@@ -1,50 +1,47 @@
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./components/pages/Home";
+import { DataProvider } from "./Globalstate";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import './App.css';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Home from './components/pages/Home';
-import { DataProvider } from './Globalstate';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
-import AdminLogin from './components/pages/AdminLogin';
-import AdminDashBoard from './components/pages/AdminDashBoard';
-import { useEffect } from 'react';
-import EditProduct from './components/products/EditProduct';
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import AdminLogin from "./components/pages/AdminLogin";
+import AdminDashBoard from "./components/pages/AdminDashBoard";
+import { useEffect } from "react";
+import EditProduct from "./components/products/EditProduct";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 function App() {
-  useEffect(()=>{
-    toast.success("أهلاً بكم في سوق أبنود")
-            
-  },[])
+  useEffect(() => {
+    toast.success("أهلاً بكم في سوق أبنود");
+  }, []);
   return (
     <div className="App">
-      
       <DataProvider>
-    <HashRouter >
-      <Header/>
-      <Routes >
+        <HashRouter>
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/admin" exact element={<AdminLogin />} />
+            <Route path="/admin-panel" exact element={<AdminDashBoard />} />
+            <Route path="/edit/:id" exact element={<EditProduct />} />
+          </Routes>
+          <Footer />
 
-        <Route path='/' exact element={<Home/>} />
-        <Route path='/admin'exact  element={<AdminLogin/>} />
-        <Route path='/admin-panel' exact element={<AdminDashBoard/>} />
-        <Route path='/edit/:id' exact element={<EditProduct/>} />
-
-      </Routes>
-      <Footer/>
-
-      <ToastContainer
-          position="top-left"
-          autoClose={1000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
+          <ToastContainer
+            position="top-left"
+            autoClose={1000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
           />
-    </HashRouter>
+        </HashRouter>
       </DataProvider>
     </div>
   );
